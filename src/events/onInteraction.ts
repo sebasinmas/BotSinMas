97a153b73bda8f625848d702693ba34974e5a006
SebaSinMas <sebastianbustos90@gmail.com>
2022-10-14 15:35:03 -0300

    first commit

---
+++ src/events/onInteraction.ts
@@ -0,0 +1,10 @@
+import { Interaction } from "discord.js"
+
+export const onInteraction = async (interaction: Interaction) => {
+    if(interaction.isCommand()){
+        
+
+    }
+
+
+}
---
+++ src/interfaces/Command.ts
@@ -0,0 +1,11 @@
+import { SlashCommandBuilder, SlashCommandSubcommandsOnlyBuilder } from "@discordjs/builders"
+
+export interface Command {
+    data:{
+
+    }
+    run:{
+
+    }
+
+}
